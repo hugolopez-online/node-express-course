3be04f42899dbae57cb31d207380da54cae13185
sketchugo <sketchugo@gmail.com>
2025-04-30 09:24:24 -0700

    setup of servir with all the resources required

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,12 +3,14 @@ console.log('Express Tutorial')
 const http = require("http");
 const PORT = 3000;
 
-//readFileSync is because it's invoked outside the server creation, and they're ready to be used: they won't get read with every coming request
+//readFileSync is because it's invoked outside the server creation, and they're ready to be used: they won't be read with every coming request
 const { readFileSync } = require("fs");
 
 //get all files
-const homePage = readFileSync("./index.html");
-const aboutPage = readFileSync("./about.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
+const homeIcon = readFileSync("./navbar-app/logo.svg");
+const homeStyles = readFileSync("./navbar-app/styles.css");
 
 const server = http.createServer((req, res) => {
     //do server stuff
@@ -19,10 +21,20 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {"content-type": "text/html"})
         res.write(homePage);
         res.end();
-    } else if (url == "/about") {
-        //if about page
-        res.writeHead(200, {"content-type": "text/html"})
-        res.write(aboutPage);
+    } else if (url == "/styles.css") {
+        //styles
+        res.writeHead(200, {"content-type": "text/css"})
+        res.write(homeStyles);
+        res.end();
+    } else if (url == "/logo.svg") {
+        //styles
+        res.writeHead(200, {"content-type": "image/svg+xml"})
+        res.write(homeIcon);
+        res.end();
+    } else if (url == "/browser-app.js") {
+        //styles
+        res.writeHead(200, {"content-type": "text/javascript"})
+        res.write(homeLogic);
         res.end();
     } else {
         //if not found
