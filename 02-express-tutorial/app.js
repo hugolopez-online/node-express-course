9bccd11242cd288539ecb8e2bba91d2020e7eab3
sketchugo <sketchugo@gmail.com>
2025-04-30 14:49:52 -0700

    queries

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,59 @@
-const express = require ("express");
-const path = require("path")
+const express = require("express");
+const { products } = require("./data")
 const app = express();
 const PORT = 3000;
 
-//setup static and middleware
-app.use(express.static("./public"))
-
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+    res.send("<h1>Home Page</h1><a href='/api/products'>products</a>");
 });
 
-app.all("*", (req, res) => {
-    res.status(404).send("Resource not found")
+app.get("/api/products", (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image }
+    });
+    res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+    // console.log(req);
+    // console.log(req.params);
+    
+    const { productID } = req.params;
+    const singleProduct = products.find((product) => product.id === Number(productID));
+    if (!singleProduct) {
+        //if doesn't exist
+        return res.status(404).send("Product does not exist")
+    }
+    res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/review/:reviewID", (req, res) => {
+    res.send("Hello world");
+    
+});
+
+app.get("/api/v1/search", (req, res) => {
+    console.log(req.query);
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        });
+    };
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    };
+
+    if (sortedProducts.length < 1) {
+        return res.status(200).send("No products available")
+    };
+    return res.status(200).json(sortedProducts);    
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
     
 });
