0a77f5bb7010b98afe029629970755b1c9f16821
sketchugo <sketchugo@gmail.com>
2025-05-01 06:56:53 -0700

    middleware and .use()

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,56 +1,26 @@
 const express = require("express");
-const { products } = require("./data")
+const logger = require("./logger");
 const app = express();
 const PORT = 3000;
 
+//use() will apply the middleware func to every request handler after it
+//and if a first argument with a path is passed, it'll apply it only to those matching that path
+app.use("/api", logger)
+
 app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1><a href='/api/products'>products</a>");
+    res.send("Home")
+});
+
+app.get("/about", (req, res) => {
+    res.send("About")
 });
 
 app.get("/api/products", (req, res) => {
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product;
-        return { id, name, image }
-    });
-    res.json(newProducts);
+    res.send("Products")
 });
 
-app.get("/api/products/:productID", (req, res) => {
-    // console.log(req);
-    // console.log(req.params);
-    
-    const { productID } = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productID));
-    if (!singleProduct) {
-        //if doesn't exist
-        return res.status(404).send("Product does not exist")
-    }
-    res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/review/:reviewID", (req, res) => {
-    res.send("Hello world");
-    
-});
-
-app.get("/api/v1/search", (req, res) => {
-    console.log(req.query);
-    const { search, limit } = req.query;
-    let sortedProducts = [...products];
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search);
-        });
-    };
-
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0,Number(limit))
-    };
-
-    if (sortedProducts.length < 1) {
-        return res.status(200).send("No products available")
-    };
-    return res.status(200).json(sortedProducts);    
+app.get("/api/items", (req, res) => {
+    res.send("Items")
 });
 
 app.listen(PORT, () => {
