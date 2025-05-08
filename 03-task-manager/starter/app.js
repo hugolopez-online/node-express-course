541bcc3b3fe71b8c54c7109cc92a047949fbfea6
sketchugo <sketchugo@gmail.com>
2025-05-08 07:41:53 -0700

    course progress (02:52:11)

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
-const tasks = require("./routes/tasks")
-const connectDB = require("./db/connect")
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.json())
+app.use(express.static("./public"));
+app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-    res.send("Task Manager App");
-});
-
-app.use("/api/v1/tasks", tasks)
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 //server init
 const PORT = 3000;
@@ -20,13 +21,12 @@ const PORT = 3000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        console.log("Connected to DB...");        
+        console.log("Connected to DB...");
         app.listen(PORT, () => {
             console.log(`Server ready and listening on port ${PORT}...`);
         });
     } catch (err) {
         console.log(err);
-        
     }
 };
 
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb+srv://sketchugo:Sunflower%2CVol.6@nodeexpressprojects.qffen0p.mongodb.net/03-TAKS-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects";
-
 const connectDB = (url) => {
     return mongoose.connect(url, {
         useNewUrlParser: true,
