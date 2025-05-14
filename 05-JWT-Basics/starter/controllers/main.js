de422982573c0e09af7f553688fad9a220b5b456
sketchugo <sketchugo@gmail.com>
2025-05-13 19:09:22 -0700

    course progress 05:24:00

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,19 @@
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        throw new CustomAPIError("Please provide email and password", 400);
+    }
+    return res.send("Fake login/register route");
+};
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+        msg: `Hello, John Doe`,
+        secret: `Here is your auth data. Your lucky number is ${luckyNumber}`,
+    });
+};
+
+module.exports = { login, dashboard };
