b6759de55479fdad3e803a6f6591058d3e2ff9be
sketchugo <sketchugo@gmail.com>
2025-05-15 10:12:17 -0700

    course progress 06:15:00

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,17 +1,32 @@
+const jwt = require("jsonwebtoken");
+
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-        throw new CustomAPIError("Please provide email and password", 400);
+        throw new CustomAPIError(
+            "Please provide email and password you wacko",
+            400
+        );
     }
-    return res.send("Fake login/register route");
+
+    const id = new Date().getDate();
+
+    //try to keep payload small (sign(<payload>, <secret>, <options>))
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+
+    res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
+    console.log(req.user);
+
     const luckyNumber = Math.floor(Math.random() * 100);
     res.status(200).json({
-        msg: `Hello, John Doe`,
+        msg: `Hello, ${req.user.username}`,
         secret: `Here is your auth data. Your lucky number is ${luckyNumber}`,
     });
 };
