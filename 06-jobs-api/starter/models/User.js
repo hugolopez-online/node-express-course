6391400f46a04f3195316ad91380187d907137d8
sketchugo <sketchugo@gmail.com>
2025-05-16 14:34:30 -0700

    course progress 07:33:00

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -20,8 +22,19 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, "please provide password"],
         minlength: 6,
-        maxlength: 12,
     },
 });
+
+UserSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
+        expiresIn: "30d",
+    });
+};
 
 module.exports = mongoose.model("User", UserSchema);
