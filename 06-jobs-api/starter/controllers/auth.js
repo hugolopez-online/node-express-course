c0104fc68f698d27e3f1fd8d47c1935bf2df9685
hugolopez-online <hugolopez.online@gmail.com>
2025-07-18 22:26:44 -0700

    chore(ci): update latest version

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
@@ -14,7 +14,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-    res.send("Login user route");
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new BadRequestError("Please provide email and password");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new UnauthenticatedError("Invalid credentials");
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError("Invalid password");
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { user: user.name }, token });
 };
 
 module.exports = { register, login };
